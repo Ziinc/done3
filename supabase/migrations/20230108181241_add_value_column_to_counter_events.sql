cd0df7553d3c18f5cefe85eafc03ed4d01c223e1
TzeYiing <ty@tzeyiing.com>
2023-01-09 02:35:13 +0800

    fix: add missing migration for bulk increase counters

---
+++ supabase/migrations/20230108181241_add_value_column_to_counter_events.sql
@@ -0,0 +1,17 @@
+alter table "public"."counter_events" add column "value" smallint not null;
+
+create or replace view "public"."view_counters" as  SELECT c.id,
+    c.created_at,
+    c.updated_at,
+    c.name,
+    c.user_id,
+    COALESCE(sum(e.value), (0)::bigint) AS count,
+    c.sort_index,
+    c.target,
+    c.notes
+   FROM (counters c
+     LEFT JOIN counter_events e ON ((e.counter_id = c.id)))
+  GROUP BY c.id, c.name, c.created_at, c.updated_at, c.user_id, c.sort_index, c.target, c.notes;
+
+
+
