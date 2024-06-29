fd10a620eb38b2e6c514d1d3450e0f5a14004f6e
TzeYiing <ty@tzeyiing.com>
2024-06-29 17:56:40 +0800

    feat: update config from pull

--- supabase/migrations/20240622055543_google_auth_table_cron_updating.sql
+++ supabase/migrations/20240622055543_google_auth_table_cron_updating.sql
@@ -186,22 +186,7 @@ grant truncate on table "cron"."job_run_details" to "postgres";
 
 grant update on table "cron"."job_run_details" to "postgres";
 
-create policy "cron_job_policy"
-on "cron"."job"
-as permissive
-for all
-to public
-using ((username = CURRENT_USER));
-
-
-create policy "cron_job_run_details_policy"
-on "cron"."job_run_details"
-as permissive
-for all
-to public
-using ((username = CURRENT_USER));
-
-
+DROP TRIGGER IF EXISTS cron_job_cache_invalidate on "cron"."job";
 CREATE TRIGGER cron_job_cache_invalidate AFTER INSERT OR DELETE OR UPDATE OR TRUNCATE ON cron.job FOR EACH STATEMENT EXECUTE FUNCTION cron.job_cache_invalidate();
 
 
