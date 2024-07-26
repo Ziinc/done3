0cf7ea1f11a4aa3e64412a1f081adcb1e78da307
TzeYiing <ty@tzeyiing.com>
2024-07-27 01:20:21 +0800

    chore: formatting

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/reset.css";
 import "./index.css";
-import {AuthProvider} from "./components/Auth";
+import { AuthProvider } from "./components/Auth";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
